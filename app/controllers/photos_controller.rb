69747ed8de26fe0404edf625ebc35ff303ad8334
Shriansh Shrivastava <shriansh@gmail.com>
2015-05-05 21:17:52 -0500

    homework complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,46 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+    @photo_id = params[:id]
+    @list_of_photos = Photo.all
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+
+    z = Photo.new
+    z.source = params[:the_source]
+    z.caption = params[:the_caption]
+    z.save
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def delete_photo
+
+    Photo.find((params[:id]).to_i).destroy
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+
+  def edit_form
+    @photo_id = params[:id].to_i
+  end
+
+  def update_row
+    z = Photo.find((params[:id]).to_i)
+    z.source = params[:the_source]
+    z.caption = params[:the_caption]
+    z.save
+    redirect_to("http://localhost:3000/photos")
+
+  end
 
 end
